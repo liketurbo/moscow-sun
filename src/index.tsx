d70538d85a31e75accfe9010e0b1749556c9470d
Ramzan Chasygov <theliketurbo@gmail.com>
2019-05-09 17:58:23 +0300

    upgraded preact to preact X

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import { h, render } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 
-render(
-  <div id="foo">
-    <span>Hello, world!</span>
-    <button onClick={() => alert('hi!')}>Click Me</button>
-  </div>,
-  document.body
-);
+const App = () => {
+  const [sunrise, setSunrise] = useState('');
+  const [sunset, setSunset] = useState('');
+
+  // @ts-ignore
+  useEffect(async () => {
+    const res = await fetch(
+      'https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today'
+    );
+    const data = await res.json();
+
+    setSunrise(data.results.sunrise);
+    setSunset(data.results.sunset);
+  });
+
+  return (
+    <main>
+      <span>{sunrise}</span> | <span>{sunset}</span>
+    </main>
+  );
+};
+
+render(<App />, document.body);
