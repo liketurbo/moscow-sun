042c5caa5eb881872106984122b1c2d130dc0cef
Ramzan Chasygov <theliketurbo@gmail.com>
2019-05-10 23:35:22 +0300

    replaced "favicon" with "emoji favicon"

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const merge = require('webpack-merge');
+const EmojiFaviconPlugin = require('emoji-favicon-webpack-plugin');
 
 const DEV_ENV = 'DEV_ENV';
 const PROD_ENV = 'PROD_ENV';
@@ -18,9 +18,6 @@ const commonConfig = env => ({
   output: {
     filename: '[contenthash].js',
     path: path.join(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -59,11 +56,7 @@ const commonConfig = env => ({
     new MiniCssExtractPlugin({
       filename: '[contenthash].css'
     }),
-    new FaviconsWebpackPlugin({
-      logo: path.join(__dirname, 'src', 'assets', 'favicon', 'sunset.png'),
-      title: 'Moscow Sun - Get time of sunrise and sunset',
-      prefix: 'favicon/'
-    })
+    new EmojiFaviconPlugin('🌇')
   ]
 });
 
