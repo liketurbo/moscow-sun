8365c2e4d36b3e6bb96c87e205c00ea586777fbc
Ramzan Chasygov <theliketurbo@gmail.com>
2019-05-15 15:55:32 +0300

    fixed bug with wrong variable

--- src/redux/sagas/decs-sun.ts
+++ src/redux/sagas/decs-sun.ts
@@ -1,13 +1,15 @@
 import { put, select } from 'redux-saga/effects';
 
-import { _DECR_SUN, _SET_SUN } from '../actions';
+import { _DECR_SUN, SET_SUN } from '../actions';
 
 function* decsSun() {
-  const remainSunrise = yield select(state => state.remainSunrise);
-  const remainSunset = yield select(state => state.remainSunrise);
+  const [remainSunrise, remainSunset] = yield select(state => [
+    state.remainSunrise,
+    state.remainSunset
+  ]);
 
   if (remainSunrise <= 0 || remainSunset <= 0) {
-    yield put({ type: _SET_SUN });
+    yield put({ type: SET_SUN });
   }
 
   yield put({ type: _DECR_SUN });
